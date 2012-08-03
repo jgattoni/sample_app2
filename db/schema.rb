5385e63246547b5931003613ccf82eb0e0e2563e
Julien Gattoni <julien.gattoni@gmail.com>
2012-08-03 18:00:17 +0200

    Finish sign in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120802151509) do
+ActiveRecord::Schema.define(:version => 20120803152006) do
 
   create_table "relationships", :force => true do |t|
     t.integer  "follower_id"
@@ -30,8 +30,10 @@ ActiveRecord::Schema.define(:version => 20120802151509) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
