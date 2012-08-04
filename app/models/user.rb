5b83adfc3ff29becb789083319f98c14a3141172
Julien Gattoni <julien.gattoni@gmail.com>
2012-08-04 05:28:32 +0200

    Change to work with Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password, :password_confirmation
+  attr_accessible :email, :name, :password, :password_confirmation, :remember_token
   has_secure_password
   
   before_save { |user| user.email = email.downcase }
