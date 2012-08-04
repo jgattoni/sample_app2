9c2246d709f79e7cc9820471fc3f2e4643f95acc
Julien Gattoni <julien.gattoni@gmail.com>
2012-08-04 16:50:05 +0200

    Chapter 9-List all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   
+  before_filter :signed_in_user,  only: [:index, :edit, :update]
+  before_filter :correct_user,    only: [:edit, :update]
+  
   def show
       @user = User.find(params[:id])
   end
@@ -19,4 +22,37 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    #@user = User.find(params[:id])
+  end
+  
+  def update
+    #@user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def index
+    @users = User.all
+  end
+  
+  private
+  
+    def signed_in_user
+      store_location
+      redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+  
 end
